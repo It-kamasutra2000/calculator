fd7fa1a525b9aeb5f41c20484be0420722602d71
David Gasparyan <davidGasparyan@gmail.com>
2022-04-14 22:38:24 +0400

    add Screen component

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
+
 import styles from './Calculator.module.scss';
+import { Screen } from './Screen/Screen';
 
 export const Calculator: FC = () => {
     return <div className={styles.calculator}>
-
+        <Screen/>
     </div>
 }
