6efbe696b4002362844503b0136cf8aba4563c5f
David Gasparyan <davidGasparyan@gmail.com>
2022-04-14 23:07:14 +0400

    add button component

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react';
+import { Button } from './Button/Button';
 
 import styles from './Calculator.module.scss';
 import { Screen } from './Screen/Screen';
 
 export const Calculator: FC = () => {
     return <div className={styles.calculator}>
-        <Screen/>
+        <Screen />
+        <div style={{ display: 'flex' }}>
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+        </div>
     </div>
 }
