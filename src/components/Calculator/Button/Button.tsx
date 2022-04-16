dd809edf82446535b32bdb57fbfd537164079de9
davit <davit@gmail.com>
2022-04-16 21:48:10 +0400

    add icons

--- src/components/Calculator/Button/Button.tsx
+++ src/components/Calculator/Button/Button.tsx
@@ -2,11 +2,14 @@ import { FC } from 'react';
 import styles from './Button.module.scss';
 
 interface IButton {
-    actionType: string
+    Icon: React.Component | any;
+    bgrCol?: string;
 }
 
-export const Button: FC = () => {
-    return <div className={styles.button}>
-
+export const Button: FC<IButton> = ({ Icon, bgrCol = '#4B4B4B' }) => {
+    return <div className={styles.button} style={{background: bgrCol}}>
+        <div className={styles.icon}>
+            <Icon />
+        </div>
     </div>
 }
--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,4 +1,7 @@
 import { FC } from 'react';
+import { Add } from '../../assets/svg/Add';
+import { Divide } from '../../assets/svg/Divide';
+import { Multiple } from '../../assets/svg/Multiple';
 import { Button } from './Button/Button';
 
 import styles from './Calculator.module.scss';
@@ -7,11 +10,24 @@ import { Screen } from './Screen/Screen';
 export const Calculator: FC = () => {
     return <div className={styles.calculator}>
         <Screen />
-        <div style={{ display: 'flex' }}>
-            <Button />
-            <Button />
-            <Button />
-            <Button />
+        <div className={styles.test}>
+            <Button Icon={Divide} />
+            <Button Icon={Add} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+            <Button Icon={Multiple} />
+         
         </div>
     </div>
 }
