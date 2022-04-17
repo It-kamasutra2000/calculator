e3737f458a31ce3a069cc7341fa9a2b63d745062
davit <davit@gmail.com>
2022-04-17 22:49:50 +0400

    add screen and buttons

--- src/components/Calculator/Button/Button.tsx
+++ src/components/Calculator/Button/Button.tsx
@@ -2,14 +2,17 @@ import { FC } from 'react';
 import styles from './Button.module.scss';
 
 interface IButton {
-    Icon: React.Component | any;
+    Icon?: React.Component | any;
     bgrCol?: string;
+    figure?: number;
 }
 
-export const Button: FC<IButton> = ({ Icon, bgrCol = '#4B4B4B' }) => {
+export const Button: FC<IButton> = ({ Icon, figure, bgrCol = '#4B4B4B' }) => {
+    const icon = figure === 0 ? 0 : <Icon/>; 
+
     return <div className={styles.button} style={{background: bgrCol}}>
-        <div className={styles.icon}>
-            <Icon />
+        <div className={`${styles.icon} ${figure ? styles.figure : ''}`}>
+            {figure || icon} 
         </div>
     </div>
 }
--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,33 +1,37 @@
 import { FC } from 'react';
-import { Add } from '../../assets/svg/Add';
-import { Divide } from '../../assets/svg/Divide';
 import { Multiple } from '../../assets/svg/Multiple';
+import { colors } from '../../utils';
 import { Button } from './Button/Button';
 
 import styles from './Calculator.module.scss';
+import { ClearBlock } from './ClearBlock/ClearBlock';
+import { Header } from './Header/Header';
 import { Screen } from './Screen/Screen';
 
 export const Calculator: FC = () => {
     return <div className={styles.calculator}>
-        <Screen />
-        <div className={styles.test}>
-            <Button Icon={Divide} />
-            <Button Icon={Add} />
+        <Header />
+        <div className={styles.screenBlock}>
+            <ClearBlock />
+            <Screen />
+        </div>
+        <div className={styles.buttons}>
+            <Button figure={7} bgrCol={colors.topFiguresColor} />
+            <Button figure={8} bgrCol={colors.topFiguresColor} />
+            <Button figure={9} bgrCol={colors.topFiguresColor} />
+            <Button Icon={Multiple} bgrCol={colors.mathActionColor} />
+            <Button figure={4} bgrCol={colors.figuresColor} />
+            <Button figure={5} bgrCol={colors.figuresColor} />
+            <Button figure={6} bgrCol={colors.figuresColor} />
+            <Button Icon={Multiple} bgrCol={colors.mathActionColor} />
+            <Button figure={1} bgrCol={colors.figuresColor} />
+            <Button figure={2} bgrCol={colors.figuresColor} />
+            <Button figure={3} bgrCol={colors.figuresColor} />
+            <Button Icon={Multiple} bgrCol={colors.mathActionColor} />
+            <Button figure={0} bgrCol={colors.figuresColor} />
             <Button Icon={Multiple} />
             <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-            <Button Icon={Multiple} />
-         
+            <Button Icon={Multiple} bgrCol={colors.mathActionColor} />
         </div>
     </div>
 }
